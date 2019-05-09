71b4cf8eae6206702b4961904245262d5ac5169d
Tom Jones <thj@freebsd.org>
2019-05-09 07:58:22 +0000

    Fully specify the dccp header

--- sys/netinet/dccp.h
+++ sys/netinet/dccp.h
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2018 Tom Jones <thj@freebsd.org>
+ * Copyright (c) 2019 Tom Jones <thj@freebsd.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
